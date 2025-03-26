2b7d435d2b47d352de6c5d9e4830c5f29173c850
amitkumar1107 <amitalwarpvt@gmail.com>
2025-03-26 17:17:00 +0530

    Initial commit

--- ecom/settings.py
+++ ecom/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2vz^hza71o%mhk851mute0itu)^(uo84g9b$1jf!r=4cf&t2=d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['TrendyKart.onrender.com', 'localhost']
 
 
 # Application definition
@@ -80,7 +80,10 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,  # Performance optimization
+    )
 }
 
 # Password validation
