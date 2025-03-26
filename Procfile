65dbc53402e25ef2015967343b6090dc5d0bb81c
amitkumar1107 <amitalwarpvt@gmail.com>
2025-03-26 16:54:52 +0530

    Initial commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn ecom.wsgi:application --bind 0.0.0.0:$PORT
+web: gunicorn ecom.wsgi:application
--- ecom/settings.py
+++ ecom/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 DEBUG = False
 
@@ -28,7 +29,7 @@ SECRET_KEY = 'django-insecure-2vz^hza71o%mhk851mute0itu)^(uo84g9b$1jf!r=4cf&t2=d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['Trendykart.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -79,12 +80,8 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {    
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
