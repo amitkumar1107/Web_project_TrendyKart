f590a9be7801f3217ec51641edd73ad5a6e68c40
Amit kumar <amitalwarpvt@gmail.com>
2025-11-08 13:58:53 +0530

    Update forms.py

--- web/forms.py
+++ web/forms.py
@@ -5,9 +5,18 @@ from django.utils.translation import gettext_lazy as _
 from.models import customer
 
 class customeregistrationform(UserCreationForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(
+        label='Password', 
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    password2 = forms.CharField(
+        label='Confirm Password', 
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    email = forms.EmailField(
+        required=True, 
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
 
     class Meta:
         model = User
@@ -15,7 +24,13 @@ class customeregistrationform(UserCreationForm):
         labels = {'email': 'Email'}
         widgets = {'username': forms.TextInput(attrs={"class": 'form-control'})}
 
-
+    # Override clean_password2 to skip Django validators
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
 class loginform(AuthenticationForm):
     username = forms.CharField(
         label='Username',  # Correct the label to "Username"
